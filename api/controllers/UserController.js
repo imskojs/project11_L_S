0b7e0fdcad46d428f1701e78fded1a800c1ba1d1
Seunghoon Ko <imskojs@gmail.com>
2016-03-07 14:27:19 +0900

    beta

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -375,8 +375,7 @@ function ask(req, res) {
   var query = queryWrapper.query;
 
   return MailService.sendToUsers(
-      // [{ email: 'joodang123@naver.com' }],
-      [{ email: 'developer@applicat.co.kr' }],
+      [{ email: 'joodang123@naver.com' }],
       'admin', { data: query },
       query.email || 'admin@applicat.co.kr'
     )
